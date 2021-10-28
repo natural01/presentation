ff3d49d9e12d0ef9fa8838211bceea62a50fad8e
natural01 <80067153+natural01@users.noreply.github.com>
2021-10-29 00:00:56 +0300

    Update script.js

--- presentsation/js/script.js
+++ presentsation/js/script.js
@@ -1,114 +1,182 @@
 const app = {
-    CommandsHistory,
-    mode,
-    presentation = {
-        selectSlides,
-        selectElements, 
-        title,
+    mode: {
+        editor: true,
+        view: false,
+    },
+    presentation: {
+        selectslides: 1234,
+        selectElements: 1234, 
+        title: 'new title',
         slides: [
             {
-                randomId,
-                background: {
-                    img: 'directory',
-                    colour: black,
-                },
-                block: {
-                    position,
-                    blockSize,
-                    elemets: {
-                        elementId,
+                slide: {
+                    randomId: 1234,
+                    background: {
                         img: 'directory',
-                        text: {
-                            size,
-                            text,
-                            font
+                        colour: '#000'
+                    },
+                    block: {
+                        position: {
+                            x: 12,
+                            y: 12
                         },
-                        Primitive: {
-                            type: {
-                                type,
-                                colourback,
-                                colourline
+                        blockSize: {
+                            widht: 12,
+                            height: 12
+                        },
+                        elemets: {
+                            elementId: 1234,
+                            img: 'directory',
+                            text: {
+                                size: 12,
+                                text: 'hello',
+                                font: 'bold'
+                            },
+                            primitive: {
+                                type: {
+                                    type: 'circle',
+                                    colourback: '#111',
+                                    colourline: '#000'
+                                }
                             }
                         }
                     }
                 }
             }
         ]
+    },
+    commandsHistory: {
+        undo: [
+            // список презентаций до изменений
+        ],
+        redo: [
+            // список презентаций после изменений
+        ]
     }
 }
 
 /**
- * @param {Editor} Editor
- * @param {Slide} Slide
- * @return {Editor}
+ * @param {editor} editor
+ * @param {slide} slide
+ * @return {editor}
  */
-function addSlide(Editor, Slide) {}
+function addSlide(editor, slide) {}
 
 /**
- * @param {Editor} Editor
- * @param {Slide} Slide
- * @param {number} randomId
- * @return {Editor}
+ * @param {editor} editor
+ * @param {string} slideId
+ * @return {editor}
  */
-function del(editor, Slide, randomId) {}
+function delSlide(editor, slideId) {}
 
 /**
- * @param {number} id
+ * @param {editor} editor
+ * @param {string} slideId
  * @param {string} colour
- * @param {string} directory
- * @return {Slide}
+ * @return {editor}
  */
-function changeBackgroundSline(id, colour, directory) {}
+function changeBackgroundColorSlide(slideId, colour, editor) {}
 
 /**
- * @param {Editor} Editor
+ * @param {editor} editor
+ * @param {string} slideId
+ * @param {string} src
+ * @return {editor}
+ */
+function changeBackgroundImgSlide(slideId, src, editor) {}
+
+/**
+ * @param {editor} editor
  * @param {string} Text
- * @return {Editor}
+ * @param {string} slideId
+ * @return {editor}
  */
-function addText(Editor, Text) {}
+function addText(slideId, editor, Text) {}
 
 /**
- * @param {number} id
+ * @param {string} slideId
+ * @param {number} idElement
  * @param {number} size
  * @param {string} font
- * @return {Editor}
+ * @return {editor}
  */
-function changeText(id, size, font) {}
+function changeText(slideId, idElement, size, font) {}
 
 /**
- * @param {Editor} Editor
- * @param {string} URL
- * @return {Editor}
+ * @param {editor} editor
+ * @param {string} src
+ * @param {string} slideId
+ * @return {editor}
  */
-function addImg(Editor, URL) {}
+function addImg(editor, src, slideId) {}
 
 /**
- * @param {Editor} Editor
+ * @param {editor} editor
  * @param {string} type
- * @return {Editor}
+ * @param {string} slideId
+ * @return {editor}
  */
-function addPrimitive(Editor, type) {}
+function addPrimitive(editor, type, slideId) {}
 
 /**
- * @param {Editor} Editor
- * @param {string} colourback
- * @param {string} colourline
- * @return {Editor}
+ * @param {editor} editor
+ * @param {string} colorline
+ * @param {string} slideId
+ * @return {editor}
  */
-function editPrimitive(Editor, colourback, colourline) {}
+function editPrimitiveColorline(editor, colorline, slideId) {}
 
 /**
- * @param {show} show 
+ * @param {editor} editor
+ * @param {string} colorback
+ * @param {string} slideId
+ * @return {editor}
+ */
+function editPrimitiveColorback(editor, colorback, slideId) {}
+
+/**
+ * @param {editor} editor
+ * @param {string} blockId
+ * @return {editor}
+ */
+function delElement(editor, blockId) {}
+
+/**
+ * @param {editor} editor
+ * @param {string} blockId
+ * @param {number} x
+ * @param {number} y
+ * @param {number} wight
+ * @param {number} height
+ * @return {editor}
+ */
+function changeBlock(editor, blockId, x, y, wight, height) {}
+
+/**
+ * @param {view} view 
  * @param {presentation} presentation
- * @return {show}
+ * @return {view}
  */
-function showmode(show, presentation) {}
-
+function changeMode(view, presentation) {}
 
 /**
- * @param {Editor} Editor
+ * @param {editor} editor
  * @param {string} position 
  * @param {number} elementId
- * @return {Editor}
+ * @return {editor}
  */
-function selectElements(Editor, position, elementId) {}
+function selectElements(editor, position, elementId) {}
+
+/**
+ * @param {presentation} presentation
+ * @param {array} undo
+ * @return {array}
+ */
+function addUndo(presentation, undo) {}
+
+/**
+ * @param {presentation} presentation
+ * @param {array} redo
+ * @return {array}
+ */
+function addRedo(presentation, redo) {}
